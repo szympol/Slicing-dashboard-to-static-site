c1645e6c0f547a7678dd3a8ac005c44c0d382301
Szymon Polanowski <szympol@hotmail.com>
2018-10-08 07:37:40 +0200

    Add grid & E-mail address validation

--- js/script.js
+++ js/script.js
@@ -151,3 +151,22 @@ unhideCalendarIconEndCell.addEventListener('mouseleave', function(){
     unhideCalendarIconEndCell.classList.remove('calendarBackground');
 });
 
+
+
+
+
+
+// E-mail address validation
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    var isFormValidate = true;
+  
+    var emailAddressInput = event.target.querySelector('input[name="email_address"]')
+    if(emailAddressInput.value.indexOf('@') < 0) {
+      isFormValidate = false;
+      emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+    }
+  
+    return !isFormValidate ? event.preventDefault() : true;
+  });
+
