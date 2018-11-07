632030e785cf636929e6723aff6018bfbab1eb66
Szymon Polanowski <szympol@hotmail.com>
2018-11-07 12:19:46 +0100

    Finish quit modal

--- js/general.js
+++ js/general.js
@@ -101,3 +101,49 @@ var chart = new Chart(ctx, {
         }
 }
 });
+
+
+//MODALS
+
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+  };
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault()
+      closeModal()
+    })
+  });
+
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal()
+    }
+  });
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal()
+    }
+  });
+
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+  }
+
+  document.getElementById('login').addEventListener('click', function () {
+    openModal('#loginModal');
+  });
+
+document.getElementById('quit').addEventListener('click', function () {
+    openModal('#quitModal');
+});
+
+    document.getElementById('chat').addEventListener('click', function () {
+        openModal('#chatModal');
+  });
